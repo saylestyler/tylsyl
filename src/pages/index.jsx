aa2980ead54c4e8fe8d4397059c7a9362054a374
Tyler <saylestyler@gmail.com>
2019-06-18 15:17:39 -0400

    standard

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,15 @@
-import React from "react";
-import Helmet from "react-helmet";
-import { graphql } from "gatsby";
-import Layout from "../layout";
-import PostListing from "../components/PostListing/PostListing";
-import SEO from "../components/SEO/SEO";
-import config from "../../data/SiteConfig";
+import React from 'react';
+import Helmet from 'react-helmet';
+import { graphql } from 'gatsby';
+import Layout from '../layout';
+// import PostListing from "../components/PostListing/PostListing";
+import SEO from '../components/SEO/SEO';
+import config from '../../data/SiteConfig';
 
 class Index extends React.Component {
   render() {
     const postEdges = this.props.data.allMarkdownRemark.edges;
+    console.log(postEdges);
     return (
       <Layout>
         <div className="index-container">

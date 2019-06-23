841587b3a2df424b0bffe81e4445b7ffb20591c0
tyler sayles <saylestyler@gmail.com>
2019-06-23 01:47:37 -0400

    add test plopped article

---
+++ content/2019-06-23/2019-06-23--what-s-nginx-really-truly-actually-easily.md
@@ -0,0 +1,19 @@
+---
+title: what&#x27;s nginx? (really truly actually easily
+date: "2019-06-23"
+tags:
+  - nginx
+  -  reverse-proxy
+  -  server
+  -  docker
+  -  netwoking
+draft: false
+---
+
+on day 250 of Bootcamp, a fellow ~soldier~ asked (innocently) 
+
+> "So... What's nginx?"
+
+the question you're (I'm) never prepared to answer! `*immediately logs into old jobs server*`, `*greps for server.conf*` (jk grepped for `nginx` first lol), shows pithy declaration (? definition?) of routes / ip addressses like so: 
+
+
---
+++ generators/post-generator.js
@@ -0,0 +1,51 @@
+const inputRequired = title => {
+	return title => { 
+		((title.length > 2) ? true : `${title} != > 2`)
+	}
+}
+
+module.exports = plop => {
+	plop.setGenerator('post entry', { prompts: [
+		{
+			type: 'input',
+			name: 'title',
+			message: 'title (date auto-generated)',
+			// validate: inputRequired('title')
+		},
+		{
+			type: 'input',
+			name: 'tags',
+			message: 'supply comma separatd list'
+		},
+		{
+			type: 'input',
+			name: 'body',
+			message: 'body text'
+		},
+		{
+			type: 'confirm',
+			name: 'draft',
+			message: 'draft? (default: true)',
+			default: true 
+		}
+	],
+		actions: data => {
+			data.date = new Date().toISOString().split('T')[0]
+
+			// Parse tags as yaml array
+			if (data.tags) {
+				data.tags = 
+					`tags:\n  - ${data.tags.split(',').join('\n  - ')}`
+			}
+
+			// Add the file
+			return [{
+					type: 'add',
+					path: '../content/{{date}}--{{dashCase title}}.md',
+					templateFile: 'templates/post-md.template'
+				}
+			]
+		},
+		// “a function that takes the answers data as a parameter and returns the actions array”.
+	})
+}

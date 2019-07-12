d1fa8731e4ab962203da488a79025302e112a718
Tyler Sayles <saylestyler@gmail.com>
2019-07-12 18:51:36 -0400

    bloop mb breaking if so, fix dom content loaded bizness :~)

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -6,42 +6,22 @@ import BlogPostPreview from './preview-templates/BlogPostPreview';
 CMS.registerPreviewTemplate('about', AboutPagePreview);
 CMS.registerPreviewTemplate('blog', BlogPostPreview);
 
-/*!
- * Run event after the DOM is ready
- * (c) 2017 Chris Ferdinandi, MIT License, https://gomakethings.com
- * @param  {Function} fn Callback function
- */
-var ready = function (fn) {
-  // Sanity check
-  if (typeof fn !== 'function') return;
+// if (document.readyState === 'interactive' || document.readyState === 'complete') {
 
-  // If document is already loaded, run method
-  if (document.readyState === 'interactive' || document.readyState === 'complete') {
-    return fn();
-  }
-
-  // Otherwise, wait until document is loaded
-  document.addEventListener('DOMContentLoaded', fn, false);
-};
-
-ready(function () {
-  // hide shit
+document.addEventListener('DOMContentLoaded', () => {
   document.body.getElementsByClassName('nc-collectionPage-topHeading')[0].style.visibility = 'hidden';
   document.body.getElementsByClassName('nc-collectionPage-sidebarHeading')[0].style.visibility = 'hidden';
-
-  // highlight search as a reminder that u can search all posts
   document.body.getElementsByClassName('nc-collectionPage-sidebarSearch')[0].style.width = '300px';
-  
-  var widg = document.createElement('div');
-  widg.innerHTML = `<div>
-                      <button id="cl-button"
-                              class="myBtn">
-                              <a href="https://www.tylsyl.com/upload">
-                              get media 
-                              </a>
-                            </button>
 
-                    </div>`;
+  var widg = document.createElement('div');
+  widg.innerHTML = `
+    <div> 
+      <button id="cl-button" class="myBtn">
+        <a href="https://www.tylsyl.com/upload">
+          get media 
+        </a>
+      </button>
+    </div>`;
   widg.style.position = 'fixed';
   widg.style.background = 'green';
   widg.style.top = '60px';
@@ -66,4 +46,5 @@ ready(function () {
   newBlogButton[0].innerHTML = '<h1>NYA</h1>';
 
   document.body.getElementsByClassName('nc-appHeader-content')[0].style.visibility = 'hidden';
-});
+},
+false);

b373f47ac456e01dce77dd599f7d944dbe5d2114
Tyler Sayles <saylestyler@gmail.com>
2019-07-24 20:30:03 -0400

    change meta and run semistandard

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,23 +1,23 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
-import Navbar from '../components/Navbar'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
+import Navbar from '../components/Navbar';
 
-import './all.sass'
+import './all.sass';
 
-require('prismjs/themes/prism-solarizedlight.css')
+require('prismjs/themes/prism-solarizedlight.css');
 
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet>
-      <meta name='description' content='tyler sayles, dev blog, web development, data dump, central dumping' />
+      <meta name='description' content='tyler sayles, central dev blog data dump' />
       <meta name='google-site-verification' content='KTnPw9JrncXlqv5zhAGJH1Bx46F553A-8HIaNk3dajY' />
     </Helmet>
     <Navbar />
     <div className='children-class'>{children()}</div>
   </div>
-)
+);
 
-TemplateWrapper.propTypes = { children: PropTypes.func }
+TemplateWrapper.propTypes = { children: PropTypes.func };
 
-export default TemplateWrapper
+export default TemplateWrapper;

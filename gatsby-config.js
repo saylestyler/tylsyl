5caa4f9b8e4997d2c81d33226a1cde0abaf682a6
Tyler Sayles <saylestyler@gmail.com>
2019-08-09 02:58:08 -0400

    fix config for deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,24 +37,24 @@ module.exports = {
         ]
       }
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: "TylSyl dev brain dump",
-        short_name: "TylerSaylesBlog",
-        start_url: "/",
-        background_color: "#FFFFFF",
-        theme_color: "#FAF",
-        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
-        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-        display: "standalone",
-        icon: "src/images/icon.png", // This path is relative to the root of the site.
-        // An optional attribute which provides support for CORS check.
-        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
-        // Any invalid keyword or empty string defaults to `anonymous`
-        crossOrigin: `use-credentials`,
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: "TylSyl dev brain dump",
+    //     short_name: "TylerSaylesBlog",
+    //     start_url: "/",
+    //     background_color: "#FFFFFF",
+    //     theme_color: "#FAF",
+    //     // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+    //     // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+    //     display: "standalone",
+    //     icon: "src/images/icon.png", // This path is relative to the root of the site.
+    //     // An optional attribute which provides support for CORS check.
+    //     // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+    //     // Any invalid keyword or empty string defaults to `anonymous`
+    //     crossOrigin: `use-credentials`,
+    //   },
+    // },
     {
       resolve: `gatsby-transformer-remark`,
       options: {

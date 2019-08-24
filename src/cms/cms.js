3f9b2af68341081a820743c71a8e62de6802243a
Tyler Sayles <saylestyler@gmail.com>
2019-08-24 17:42:19 -0400

    cloudinary-cms lib

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,10 +1,7 @@
 import CMS from 'netlify-cms';
-import cloudinary from 'netlify-cms-media-library-cloudinary';
 
 import AboutPagePreview from './preview-templates/AboutPagePreview';
 import BlogPostPreview from './preview-templates/BlogPostPreview';
-
-CMS.registerMediaLibrary(cloudinary);
 
 CMS.registerPreviewTemplate('about', AboutPagePreview);
 CMS.registerPreviewTemplate('blog', BlogPostPreview);

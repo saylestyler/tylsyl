21fca98250ab5b8d885fce86f88af4f7190c01d7
Tyler Sayles <saylestyler@gmail.com>
2019-11-02 19:34:39 -0400

    standard

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -6,22 +6,3 @@ import BlogPostPreview from './preview-templates/BlogPostPreview';
 CMS.registerPreviewTemplate('about', AboutPagePreview);
 CMS.registerPreviewTemplate('blog', BlogPostPreview);
 
-window.onload = () => {
-  /* hide stuff */
-  // tar borta poänglösa navbar
-  document.querySelector(`#nc-root > div > div.nc-appHeader-container`).innerText = ''
-  // contents sidebar
-  document.querySelector('#nc-root > div > div.nc-app-main > div > div > div.nc-collectionPage-sidebar').style.width = '0px'
-  // view as grid
-  document.querySelector('#nc-root > div > div.nc-app-main > div > div > div.nc-collectionPage-main > div.nc-collectionPage-top > div.nc-collectionPage-top-viewControls.nc-collectionPage-top-viewControls-noDescription').innerHTML = '';
-  // sub-subtitle
-  document.querySelector('#nc - root > div > div.nc - app - main > div > div > div.nc - collectionPage - main > div.nc - collectionPage - top > div.nc - collectionPage - top - row > h1').innerHTML = '';
-
-  /* mod */
-  // nice n shiny new button
-  document.querySelector(`#nc-root > div > div.nc-app-main > div > div > div.nc-collectionPage-main > div.nc-collectionPage-top > div.nc-collectionPage-top-row > a`).innerText = 'NEW'
-  document.querySelector(`#nc-root > div > div.nc-app-main > div > div > div.nc-collectionPage-main > div.nc-collectionPage-top > div.nc-collectionPage-top-row > a`).style.backgroundColor = 'blue'
-
-  document.querySelector('.nc-collectionPage-topNewButton').style.padding = '0 180px';
-};
-

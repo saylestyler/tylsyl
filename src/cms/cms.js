c7e225e64ec13e14bb4572f2c2c036ecc257a0f5
Tyler Sayles <saylestyler@gmail.com>
2019-11-02 19:08:40 -0400

    standard

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -23,16 +23,16 @@ window.onload = () => {
   /* hide stuff */
   // tar borta poänglösa navbar
   // contents sidebar
-  document.querySelector('#nc-root > div > div.nc-app-main > div > div > div.nc-collectionPage-sidebar').style.width = '0px'
-  // view as grid
-  document.querySelector('#nc-root > div > div.nc-app-main > div > div > div.nc-collectionPage-main > div.nc-collectionPage-top > div.nc-collectionPage-top-viewControls.nc-collectionPage-top-viewControls-noDescription').innerHTML = '';
-  // sub-subtitle
-  document.querySelector('#nc - root > div > div.nc - app - main > div > div > div.nc - collectionPage - main > div.nc - collectionPage - top > div.nc - collectionPage - top - row > h1').innerHTML = '';
+  //document.querySelector('#nc-root > div > div.nc-app-main > div > div > div.nc-collectionPage-sidebar').style.width = '0px'
+  //// view as grid
+  //document.querySelector('#nc-root > div > div.nc-app-main > div > div > div.nc-collectionPage-main > div.nc-collectionPage-top > div.nc-collectionPage-top-viewControls.nc-collectionPage-top-viewControls-noDescription').innerHTML = '';
+  //// sub-subtitle
+  //document.querySelector('#nc - root > div > div.nc - app - main > div > div > div.nc - collectionPage - main > div.nc - collectionPage - top > div.nc - collectionPage - top - row > h1').innerHTML = '';
 
-  /* mod */
-  // nice n shiny new button
-  document.querySelector(`#nc-root > div > div.nc-app-main > div > div > div.nc-collectionPage-main > div.nc-collectionPage-top > div.nc-collectionPage-top-row > a`).innerText = 'NEW'
-  document.querySelector(`#nc-root > div > div.nc-app-main > div > div > div.nc-collectionPage-main > div.nc-collectionPage-top > div.nc-collectionPage-top-row > a`).style.backgroundColor = 'blue'
-  document.querySelector('.nc-collectionPage-topNewButton').style.padding = '0 180px';
+  //[> mod <]
+  //// nice n shiny new button
+  //document.querySelector(`#nc-root > div > div.nc-app-main > div > div > div.nc-collectionPage-main > div.nc-collectionPage-top > div.nc-collectionPage-top-row > a`).innerText = 'NEW'
+  //document.querySelector(`#nc-root > div > div.nc-app-main > div > div > div.nc-collectionPage-main > div.nc-collectionPage-top > div.nc-collectionPage-top-row > a`).style.backgroundColor = 'blue'
+  //document.querySelector('.nc-collectionPage-topNewButton').style.padding = '0 180px';
 };
 

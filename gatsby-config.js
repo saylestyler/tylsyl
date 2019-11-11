93a1d6c686ee3a8dbc45777e0e51b96703f5f9ef
Tyler Sayles <saylestyler@gmail.com>
2019-11-10 20:05:15 -0500

    add react-modal-routing for about popup

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
   },
   // pathPrefix: `/`
   plugins: [
+    `gatsby-plugin-modal-routing`,
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
     'gatsby-plugin-netlify-cache',
